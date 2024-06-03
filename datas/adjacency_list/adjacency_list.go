0cf9d378749ca0bff422cb1e858564da8769ed09
if <1136156543@qq.com>
2024-06-04 01:39:48 +0800

    feat:添加领接表的数据结构与方法，待实现

--- datas/adjacency_list/adjacency_list.go
+++ datas/adjacency_list/adjacency_list.go
@@ -1,3 +1,33 @@
 package ajl
 
 // 邻接表
+type AJL struct {
+	ns         []*node
+	isDirected bool // 有向图还是无向图
+}
+
+type node struct {
+	id    any
+	neLen int // cur -> next 边的权重
+	ne    *node
+}
+
+// 初始化
+func (o *AJL) Init() {
+
+}
+
+// 将u,v建边, 长度为w
+func (o *AJL) Add(u any, v any, w int) {
+
+}
+
+// 将u,v边删除
+func (o *AJL) Remove(u any, v any) {
+
+}
+
+// 遍历领接表
+func (o *AJL) Range(f func(any) bool) {
+
+}
