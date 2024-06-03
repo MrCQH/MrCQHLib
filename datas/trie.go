7af390c8df0fbb1a9d3abf0ef7cacc3f9131bf1b
if <1136156543@qq.com>
2024-06-04 00:31:25 +0800

    feat:实现Trie

--- datas/trie.go
+++ datas/trie.go
@@ -1,3 +1,65 @@
 package datas
 
 // 字典树
+type Trie struct {
+	root *Node
+}
+
+type Node struct {
+	exist bool
+	ne    map[byte]*Node
+}
+
+func NewTrie() Trie {
+	return Trie{
+		root: &Node{
+			exist: false,
+			ne:    make(map[byte]*Node, 26),
+		},
+	}
+}
+
+func (this *Trie) Insert(word string) {
+	pc := this.root
+	for i := range word {
+		if pc.ne[word[i]] == nil {
+			pc.ne[word[i]] = new(Node)
+			pc.ne[word[i]].ne = make(map[byte]*Node)
+		}
+		pc = pc.ne[word[i]]
+	}
+	pc.exist = true
+}
+
+func (this *Trie) Search(word string) bool {
+	pc := this.root
+	for i := range word {
+		if pc.ne[word[i]] == nil {
+			return false
+		}
+		if _, ok := pc.ne[word[i]]; !ok {
+			return false
+		} else {
+			pc = pc.ne[word[i]]
+		}
+	}
+	if !pc.exist {
+		return false
+	}
+	return true
+}
+
+func (this *Trie) StartsWith(prefix string) bool {
+	pc := this.root
+	for i := range prefix {
+		if pc.ne[prefix[i]] == nil {
+			return false
+		}
+		if _, ok := pc.ne[prefix[i]]; !ok {
+			return false
+		} else {
+			pc = pc.ne[prefix[i]]
+		}
+	}
+	return true
+}
---
+++ datas/trie_test.go
@@ -0,0 +1,16 @@
+package datas
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTrie(t *testing.T) {
+	trie := NewTrie()
+	trie.Insert("abc")
+	trie.Insert("acb")
+	if !trie.Search("abc") {
+		t.Error("abc exist", trie.Search("abc"))
+	}
+	fmt.Println(trie.StartsWith("db"))
+}
